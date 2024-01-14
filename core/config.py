c0508747f067b6e1cbe7f09db341d111fce87610
xZephy <rafikulmondal836@gmail.com>
2024-01-15 02:07:32 +0530

    +Update

--- core/config.py
+++ core/config.py
@@ -1,6 +1,8 @@
 DEFAULT_PREFIX: str = ">"
 
-BOT_TOKEN: str = "MTE2MTIzODg0NzU2MTE1NDU3MA.GTHn0u.90KDIofyxlejWBDaFgv8FPa4agr9G8G7j-EDrk"
+BOT_TOKEN: str = ""
+
+# If you are using Mongo Db then add the mongo url here
 # DATABASE_URL : str =  ""
 
 
